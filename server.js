d7f34507e8b27fc5bbd96dda3b7b243323778023
swiichy <boomheadshotmn@gmail.com>
2021-06-15 08:38:16 +0200

    .

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.use(express.static('build'))
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-app.listen(process.env.PORT || 3002, () => {
-    console.log(`listening on port ${process.env.PORT || 3002}`)
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
